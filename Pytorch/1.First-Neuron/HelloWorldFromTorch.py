7c1a93d6b08fd3e6b5017db22080ecefa73722b0
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2025-06-18 11:04:50 +0530

    Basics in Deep Learning

---
+++ Pytorch/1.First-Neuron/HelloWorldFromTorch.py
@@ -0,0 +1,4 @@
+import torch
+
+print("Hello World from Torch!")
+print("The version of Torch is  : " ,torch.__version__)
---
+++ Pytorch/1.First-Neuron/manual.py
@@ -0,0 +1,7 @@
+#basic Neuron for C to F conversion
+b = 32
+w1 = 1.8
+
+x1 = int(input("Enter the Celcius in Degress:"))
+y_pred = 1*b + x1*w1
+print("Celcius to Faranheit is :", y_pred)
