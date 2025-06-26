1bebd08f5cf851dfc13f06d14d5eb7a0a4daee45
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2025-06-26 11:06:11 +0530

    Basics of Tensor

---
+++ Pytorch/1.First-Neuron/Matrix.py
@@ -0,0 +1,14 @@
+import torch
+from torch import nn
+
+
+X = torch.tensor([
+    [10],
+    [38],
+    [100],
+    [150],
+], dtype = torch.float32)
+
+X = X.type(torch.int64)
+print(X)
+print(X.dtype)
---
+++ Pytorch/1.First-Neuron/tensor_martix.py
@@ -0,0 +1,11 @@
+import torch
+
+
+X = torch.tensor([
+    [10],
+    [38],
+    [100],
+    [150],
+])
+
+print(X[:,0])
